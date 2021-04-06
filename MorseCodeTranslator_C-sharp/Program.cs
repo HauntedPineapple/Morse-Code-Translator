684041c71f74ad90466ba4fe87e8fab626cdef8c
Audrey Main <meilong.main@gmail.com>
2021-04-06 08:48:33 -0400

    Added project files

---
+++ MorseCodeTranslator_C-sharp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeTranslator_C_sharp
+{
+    class Program
+    {
+        // NOTES:
+        // this translator uses INTERNATIONAL morse code
+        // a PERIOD "." will be used to represent DOTS
+        // a HYPHEN "-" will be used to represent DASHES
+        // LETTERS are separated by SPACES
+        // WORDS are separated by FORWARD SLASHES "/"
+        // if a letter cannot be translated, it will be output as "#"
+
+        static Dictionary<char, string> morseDictionary;
+
+        static void Main(string[] args)
+        {
+            morseDictionary = new Dictionary<char, string>()
+            {
+                { 'a',".-"}, { 'b',"-..."}, { 'c',"-.-."}, { 'd',"-.."},
+                { 'e',"."}, { 'f',"..-."}, { 'g',"--."}, { 'h',"...."},
+                { 'i',".."}, { 'j',".---"}, { 'k',"-.-"}, { 'l',".-.."},
+                { 'm',"--"}, { 'n',"-."}, { 'o',"---"}, { 'p',".--."},
+                { 'q',"--.-"}, { 'r',".-."}, { 's',"..."}, { 't',"-"},
+                { 'u',"..-"}, { 'v',"...-"}, { 'w',".--"}, { 'x',"-..-"},
+                { 'y',"-.--"}, { 'z',"--.."},
+                { '0',"-----"}, { '1',".----"},  { '2',"..---"},
+                { '3',"...--"}, { '4',"....-"}, { '5',"....."}, { '6',"-...."},
+                { '7',"--..."}, { '8',"---.."}, { '9',"----."}, 
+                { '.',".-.-.-"},
+                { ',',"--..--"}, { '?',"..--.."}, { '!',"-.-.--"}, { '\'',".----."},
+                { '"',".-..-."}, { '(',"-.--.-"}, { ')',"-.--."}, { '&',".-..."},
+                { ':',"---..."}, { ';',"-.-.-."}, { '/',"-..-."}, { '_',"..--.-"},
+                { '=',"-...-"}, { '+',".-.-."}, { '-',"-....-"}, { '@',".--.-."}
+            };
+        }
+
+        static string morseToEnglish(string message)
+        {
+            return null;
+        }
+
+        static string englishToMorse(string message)
+        {
+            return null;
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,18 @@
+MORSE CODE TRANSLATOR
+========================================================
+Task: Write two functions, one that takes in a string of morse code and translates it into the English alphabet and one that does the opposite. 
+For the input, each word in morse should be distinguished by a single space and each letter in morse should be distinguished by a forward slash, “/” with spaces on either side of it.
+========================================================
+Examples: 
+	morseToEnglish(“.... . .-.. .-.. --- / .-- --- .-. .-.. -..”) ➞ “Hello world”
+	englishToMorse(“I love code”) ➞ ” .. / .-.. --- ...- . / -.-. --- -.. .”
+========================================================
+Notes:
+         this translator uses INTERNATIONAL morse code
+         a PERIOD "." will be used to represent DOTS
+         a HYPHEN "-" will be used to represent DASHES
+         LETTERS are separated by SPACES
+         WORDS are separated by FORWARD SLASHES "/"
+         if a letter cannot be translated, it will be output as "#"
+========================================================
+https://morsecode.world/international/morse2.html
